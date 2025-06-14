558851617c3d6aa7370a68b40b86256e19569e22
Mark J. Price <markjprice@gmail.com>
2025-06-14 08:40:16 +0100

    Update NorthwindContextLogger.cs

--- code/ModernWeb/Northwind.DataContext.Sqlite/NorthwindContextLogger.cs
+++ code/ModernWeb/Northwind.DataContext.Sqlite/NorthwindContextLogger.cs
@@ -6,11 +6,16 @@ public class NorthwindContextLogger
 {
   public static void WriteLine(string message)
   {
+    string folder = Path.Combine(GetFolderPath(
+      SpecialFolder.DesktopDirectory), "book-logs");
+
+    if (!Directory.Exists(folder))
+      Directory.CreateDirectory(folder);
+
     string dateTimeStamp = DateTime.Now.ToString(
       "yyyyMMdd_HHmmss");
 
-    string path = Path.Combine(GetFolderPath(
-      SpecialFolder.DesktopDirectory), 
+    string path = Path.Combine(folder, 
       $"northwindlog-{dateTimeStamp}.txt");
 
     StreamWriter textFile = File.AppendText(path);
