160768ef4b373bc21d2e69f29bac5cd11394ace6
Mark J Price <markjprice@gmail.com>
2025-09-15 09:26:25 +0100

    Add HTTP pipeline delegate

--- code/ModernWeb/Northwind.Web/Program.cs
+++ code/ModernWeb/Northwind.Web/Program.cs
@@ -19,13 +19,39 @@ if (!app.Environment.IsDevelopment())
   app.UseHsts();
 }
 
+// Implementing an anonymous inline delegate as middleware
+// to intercept HTTP requests and responses.
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+  RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+  if (rep is not null)
+  {
+    WriteLine($"Endpoint name: {rep.DisplayName}");
+    WriteLine($"Endpoint route pattern: {rep.RoutePattern.RawText}");
+  }
+
+  if (context.Request.Path == "/bonjour")
+  {
+    // In the case of a match on URL path, this becomes a terminating
+    // delegate that returns so does not call the next delegate.
+    await context.Response.WriteAsync("Bonjour Monde!");
+    return;
+  }
+
+  // We could modify the request before calling the next delegate.
+  await next();
+
+  // We could modify the response after calling the next delegate.
+});
+
 app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
 app.UseDefaultFiles(); // index.html, default.html, and so on.
-app.MapStaticAssets(); // .NET 9 or later.
-//app.UseStaticFiles(); // .NET 8 or earlier.
+//app.MapStaticAssets(); // .NET 9 or later.
+app.UseStaticFiles(); // .NET 8 or earlier.
 
 app.MapRazorComponents<App>();
 
