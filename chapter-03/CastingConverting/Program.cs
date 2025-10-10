9b47ed254775176c45439ac7580cd5d0f6b3041f
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-10 19:32:40 -0400

    Enhance number handling and string conversion logic
    Added `using System.Object` and `System.Globalization` for extended functionality. Demonstrated implicit casting of `int` to `double` and formatted output. Enhanced `doubles` array processing with `ToInt32` conversions and added a table display. Explained Banker's Rounding and implemented `Math.Round` with `MidpointRounding.AwayFromZero`. Added examples for converting various types to strings using `ToString`. Created a placeholder region for parsing strings to numbers or dates.

--- chapter-03/CastingConverting/Program.cs
+++ chapter-03/CastingConverting/Program.cs
@@ -1,4 +1,6 @@
 ﻿using static System.Convert;
+using static System.Object;
+using System.Globalization; //To use culture info+
 
 #region Casting numbers implicitly and explicitly
 int a = 10;
@@ -36,3 +38,47 @@ double[,] doubles = {
   { -9.49, -9.5, -9.51 }
 };
 
+Console.WriteLine($"| double | ToInt32 | double | ToInt32 | double | ToInt32 |");
+for (int x = 0; x < 8; x++)
+{
+    for (int y = 0; y < 3; y++)
+    {
+        Console.Write($"| {doubles[x, y],6} | {ToInt32(doubles[x, y]),7} ");
+    }
+    Console.WriteLine("|");
+}
+Console.WriteLine();
+
+//-12.5 will round to -12 towards 0 as its a positive numnber and -9.5 will round to -10 away from 0 as its negative 
+//This is known as Banker's Rounding
+#endregion
+
+#region Taking Control of Rounding Rules
+foreach (double n in doubles)
+{
+    Console.WriteLine(format:
+      "Math.Round({0}, 0, MidpointRounding.AwayFromZero) is {1}",
+      //Code below will round all positive and negative numbers away from zero
+      arg0: n,
+      arg1: Math.Round(value: n, digits: 0,
+              mode: MidpointRounding.AwayFromZero));
+}
+#endregion
+
+#region Converting From any type to a String
+int number = 12;
+Console.WriteLine(number.ToString());
+
+bool boolean = true;
+Console.WriteLine(boolean.ToString());
+
+DateTime now = DateTime.Now;
+Console.WriteLine(now.ToString());
+
+object me = new();
+Console.WriteLine(me.ToString());
+#endregion
+
+#region Parsing from Strings to Numbers or Dates and Times
+
+#endregion
