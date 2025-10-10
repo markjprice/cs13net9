357bcf8ec3e45e90198c788930e837e3c2746333
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-10 18:47:27 -0400

    Add examples for casting, converting, and rounding numbers
    Removed the initial template code and added examples to demonstrate:
    - Implicit and explicit casting between numeric types.
    - Conversion of `double` to `int` using `Convert.ToInt32`.
    - Rounding behavior for positive and negative numbers, including
      edge cases around midpoint values.

--- chapter-03/CastingConverting/Program.cs
+++ chapter-03/CastingConverting/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Convert;
+
+#region Casting numbers implicitly and explicitly
+int a = 10;
+double b = a; //int can be casted to a double 
+Console.WriteLine($"a is {a} and b is {b}");
+
+double c = 9.8;
+int d = (int)c; //use casting opeator to explictiy cast down 
+Console.WriteLine($"c is {c} and d id {d}");
+
+long e = 10;
+int f = (int)e;
+Console.WriteLine($"e is {e} and f is {f}");
+
+e = long.MaxValue;
+f = (int)e;
+Console.WriteLine($"e is {e} and f is {f}");
+#endregion
+
+#region Converting with Convert type
+double g = 9.8;
+int h = ToInt32(g);
+Console.WriteLine($" g is {g} and h is {h}");
+#endregion
+
+
+#region Rounding Numbers and default rounding rules
+double[,] doubles = {
+  { 9.49, 9.5, 9.51 },
+  { 10.49, 10.5, 10.51 },
+  { 11.49, 11.5, 11.51 },
+  { 12.49, 12.5, 12.51 },
+  { -12.49, -12.5, -12.51 },
+  { -11.49, -11.5, -11.51 },
+  { -10.49, -10.5, -10.51 },
+  { -9.49, -9.5, -9.51 }
+};
+
