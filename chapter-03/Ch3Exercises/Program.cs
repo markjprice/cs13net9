7656bd25697459663332530496b4c215947347af
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-20 12:22:42 -0400

    Add .NET 9.0 project and implement loops and FizzBuzz
    Added `Ch3Exercises.csproj` targeting .NET 9.0 with `Exe` output,
    `ImplicitUsings`, and `Nullable` enabled. Implemented a `Loops
    and Overflow` section in `Program.cs` to handle byte overflow
    safely. Added a `FizzBuzz` implementation to print numbers or
    FizzBuzz-related strings based on divisibility rules.

---
+++ chapter-03/Ch3Exercises/Program.cs
@@ -0,0 +1,53 @@
+﻿#region Loops and Overflow
+int max = 500;
+for (byte i = 0; i < max;)
+{
+    Console.WriteLine(i);
+
+    try
+    {
+        checked
+        {
+            i++;
+        }
+    }
+    catch (OverflowException)
+    {
+        //If increment ocerflow the byte, break to avoid infinite loop
+        break;
+    }
+}
+
+#endregion
+
+#region FizzBuzz
+
+int maxNumber = 100;
+
+bool buzz = false;
+bool fizz = false;
+
+for (int i = 1;  i < maxNumber; i++)
+{
+    fizz = i % 3 == 0;
+    buzz = i % 5 == 0;
+
+    if (fizz && buzz)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if (fizz)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else if (buzz)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else
+    {
+        Console.WriteLine(i);
+    }
+}
+
+#endregion
