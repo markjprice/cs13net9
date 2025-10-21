aef40948751bed278aa94f01bf3683fb12670f33
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-21 10:52:56 -0400

    Add project setup and input validation with error handling
    Added a new `.csproj` file to configure the project targeting `.NET 9.0` with `ImplicitUsings` and `Nullable` enabled. Implemented functionality in `Program.cs` to prompt the user for two numbers, validate inputs, and handle errors such as invalid input or division by zero. Fixed missing semicolons and corrected spelling errors in error messages.

---
+++ chapter-03/ExceptionHandleExercise/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine("Enter a number between 0 and 255: ");
+]
+byte min = 0;
+byte max = 255;
+byte output1, output2, result;
+
+while (true)
+{
+    string input1 = Console.ReadLine();
+
+    if (byte.TryParse(input1, out output1))
+    {
+        if (output1 >= min && output1 <= max)
+        {
+            Console.WriteLine("Enter another number between 0 and 255: ");
+            break
+        }
+        else
+        {
+            Console.WriteLine($"{output1} is out of bounds");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{input1} is not a valid ineger");
+    }
+}
+
+while (true)
+{
+    string input2 = Console.ReadLine();
+
+    if (byte.TryParse(input2, out output2))
+    {
+        if (output2 >= min && output2 <= max)
+        {
+            break
+        }
+        else
+        {
+            Console.WriteLine($"{output2} is out of bounds");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{input2} is not a valid ineger");
+    }
+}
+
+try
+{
+    result = output1 / output2;
+    Console.WriteLine();
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
