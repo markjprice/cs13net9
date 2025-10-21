aac898d76fab747aa40a772d5fc6fba1ce43a317
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-21 13:11:15 -0400

    Add TimesTable function and update project structure
    Replaced "Hello, World!" in Program.cs with a call to the
    newly added `TimesTable` function, which generates a
    multiplication table. Introduced `Program.Functions.cs`
    to define the `TimesTable` method.
    
    Fixed formatting inconsistencies in WritingFunctions.csproj
    without altering functionality.

---
+++ chapter-04/WritingFunctions/Program.Functions.cs
@@ -0,0 +1,17 @@
+﻿
+using static System.Console;
+
+partial class Program
+{
+    static void TimesTable(byte number, byte size = 12)
+    {
+        WriteLine($"This is the {number} times table with {size} rows: ");
+        WriteLine();
+
+        for (int row = 1; row <= size; row++)
+        {
+            WriteLine($"{row} x {number} = {row * number}");
+        }
+        WriteLine();
+    }
+}
