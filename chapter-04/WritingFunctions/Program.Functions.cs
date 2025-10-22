1ea27dd0696448b67b11bbad602ffb2b28aa9cfa
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-22 14:16:03 -0400

    Fix Factorial call; update project and console output
    Corrected Factorial function call in RunFactorial method.
    Commented out ConfigureConsole and tax calculation in Program.cs.
    Added Debugging.csproj for .NET 9.0 with static Console using.
    Included "Hello, World!" output in Program.cs.

--- chapter-04/WritingFunctions/Program.Functions.cs
+++ chapter-04/WritingFunctions/Program.Functions.cs
@@ -72,7 +72,7 @@ partial class Program
     {
         for (int i = 1; i <= 15; i++)
         {
-            Console.WriteLine($"{i}! = {Factorial{i}: NO}")
+            Console.WriteLine($"{i}! = {Factorial(i):NO}");
         }
     }
 
--- chapter-04/WritingFunctions/Program.cs
+++ chapter-04/WritingFunctions/Program.cs
@@ -1,5 +1,7 @@
 ﻿//TimesTable(7, 10); 
 
-ConfigureConsole();
-decimal taxToPay = CalculateTax(149, "FR");
-Console.WriteLine($"You must pay {taxToPay:C} in tax");
+//ConfigureConsole();
+//decimal taxToPay = CalculateTax(149, "FR");
+//Console.WriteLine($"You must pay {taxToPay:C} in tax");
+
+RunFactorial();
