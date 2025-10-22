c616bac537f30abc7001d3950f1c2cbf078d41d9
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-22 14:41:29 -0400

    Add math operation with deliberate bug
    Replaced "Hello, World!" with code to perform a math operation using two double variables. Introduced an `Add` function that incorrectly multiplies instead of adding the numbers. Added user prompt to press Enter to end the application.

--- chapter-04/Debugging/Program.cs
+++ chapter-04/Debugging/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double a = 4.5;
+double b = 2.5;
+double answer = Add(a, b);
+
+WriteLine($"{a} + {b} = {answer}");
+WriteLine("Press Enter to end the app.");
+ReadLine(); // Wait for user to press Enter.
+
+// Functions in Program.cs must be at the bottom of the file.
+double Add(double a, double b)
+{
+    return a + b; // deliberate bug!
+}
